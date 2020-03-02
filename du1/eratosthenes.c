b7782c70283492b75d7b7567593148feb4e49250
Tom Milostny <tommilostny@live.com>
2020-03-02 17:14:20 +0100

    fixed Eratosthenes

--- du1/eratosthenes.c
+++ du1/eratosthenes.c
@@ -5,11 +5,11 @@
 void Eratosthenes(bitset_t pole)
 {
 	bitset_index_t end_bit = sqrt(bitset_size(pole));
-	for (bitset_index_t i = 2; i <= end_bit; i++)
+	for (bitset_index_t i = 2; i < end_bit; i++)
 	{
 		if (!bitset_getbit(pole, i))
 		{
-			for (bitset_index_t j = i * i; j < bitset_size(pole); j += i)
+			for (bitset_index_t j = i * i; j <= bitset_size(pole); j += i)
 				bitset_setbit(pole, j, 1);
 		}
 	}
