c410b80ec44bc1b188005e5ba73c9986f42401e7
Tom Milostny <tommilostny@live.com>
2020-04-12 12:09:07 +0200

    added private functions, getting ready for final

--- du2/wordcount.c
+++ du2/wordcount.c
@@ -9,21 +9,28 @@
 
 #include <stdio.h>
 #include "htab.h"
-#include "htab_types.h"
+
+#define MAX_WORD_LENGTH 127
 
 int main()
 {
+	//TODO: rozmyslet si tuto velikost a odůvodnit to
 	htab_t *table = htab_init(8);
 
-	char word[256];
-	while(scanf("%s", word) != EOF)
+	char word[MAX_WORD_LENGTH + 1];
+	while(scanf("%s", word) != EOF) //TODO: nahradit get_word z modulu io.c
 	{
-		htab_lookup_add(table, word).ptr->data++;
+		//Nalezení/přidání slova do tabulky
+		htab_iterator_t i = htab_lookup_add(table, word);
+
+		//Inkrementace nalezené hodnoty (nová hodnota jako 0 + 1)
+		htab_iterator_set_value(i, htab_iterator_get_value(i) + 1);
 	}
 
+	//Výpis nalezených slov - klíč, hodnota
 	for (htab_iterator_t i = htab_begin(table); htab_iterator_valid(i); i = htab_iterator_next(i))
 	{
-		printf("%s\t%d\n", i.ptr->key, i.ptr->data);
+		printf("%s\t%d\n", htab_iterator_get_key(i), htab_iterator_get_value(i));
 	}
 
 	htab_free(table);
